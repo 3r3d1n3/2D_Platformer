9030bc042736efaf2d483559c7868f998fe3ef44
3r3d1n3 <sadie5118@naver.com>
2025-12-03 13:06:33 +0900

    1203 오전
    조이스틱 ui 및 컨트롤 구현

---
+++ Assets/Scripts/JoyStick.cs
@@ -0,0 +1,54 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class JoyStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    private Vector2 startPos, currentPos;
+    private float maxDistance = 75f;
+    private PlayerMovement playerMovement;
+    [SerializeField] private GameObject backgroundUI;
+
+    [SerializeField] private GameObject handleUI;
+    [SerializeField] private Button jumpButton;
+
+    private void Awake()
+    {
+        playerMovement = FindFirstObjectByType<PlayerMovement>();
+        jumpButton.onClick.AddListener(playerMovement.Jump);
+    }
+
+    void Start()
+    {
+        backgroundUI.SetActive(false);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        backgroundUI.SetActive(true);
+        backgroundUI.transform.position = eventData.position;
+        startPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        currentPos = eventData.position;
+        Vector2 dragDir = currentPos - startPos;
+        dragDir = Vector2.ClampMagnitude(dragDir, maxDistance);
+        
+        playerMovement.InputJoystick(dragDir.normalized.x, dragDir.normalized.y);
+        currentPos = startPos + dragDir;
+
+        handleUI.transform.position = currentPos;
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        playerMovement.InputJoystick(0f,0f);
+        handleUI.transform.localPosition = Vector3.zero;
+        backgroundUI.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,29 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public enum InputType {Keyboard, Joystick}
+    public InputType inputType;
     public SpriteRenderer playerSR;
     public Animator playerAnim;
     private Rigidbody2D playerRb;
+    private CapsuleCollider2D playerCollider;
     
-    private float horizontalInput;
-    private float verticalInput;
-    private bool isGrounded;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
+    [SerializeField] private bool isGrounded;
     private int facingDirection = 1;
 
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 3f;
 
-    [SerializeField] private float jumpPower;
+    [SerializeField] private float jumpPower = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void FixedUpdate()
@@ -29,14 +34,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        if (inputType == InputType.Keyboard)
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+        }
         
-        moveSpeed = verticalInput < 0 ? 3.5f : 7f;
+        
+        moveSpeed = verticalInput < 0 ? 1.5f : 3f;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
         
         SetPlayerAnim();
     }
 
+    public void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+        playerAnim.SetTrigger("Jump");
+    }
     
 
     private void Move()
@@ -58,8 +77,67 @@ public class PlayerMovement : MonoBehaviour
             facingDirection = horizontalInput > 0 ? 1 : -1;
             playerSR.flipX = facingDirection == -1;
         }
+
+        if (verticalInput <0)
+        {
+            playerCollider.offset = new Vector2(playerCollider.offset.x, 0.6f);
+            playerCollider.size = new Vector2(playerCollider.size.x, 1.1f);
+        }
+        else
+        {
+            playerCollider.offset = new Vector2(playerCollider.offset.x, 0.8f);
+            playerCollider.size = new Vector2(playerCollider.size.x, 1.5f);
+        }
         
         playerAnim.SetFloat("AxisX", horizontalInput);
         playerAnim.SetFloat("AxisY", verticalInput);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            playerAnim.SetBool("IsGrounded", true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            playerAnim.SetBool("IsGrounded", false);
+        }
+    }
+
+    public void InputJoystick(float h, float v)
+    {
+        if (inputType == InputType.Joystick)
+        {
+            if (v > 0.45f)
+            {
+                verticalInput = 1f;
+            }
+            else if (v < -0.45f)
+            {
+                verticalInput = -1f;
+            }
+            else
+            {
+                verticalInput = 0f;
+            }
+
+            if (h > 0.45f)
+            {
+                horizontalInput = 1f;
+            }
+            else if (h < -0.45f)
+            {
+                horizontalInput = -1f;
+            }
+            else horizontalInput = 0f;
+        }
+        
     }
 }
